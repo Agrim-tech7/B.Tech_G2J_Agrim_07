178a524c2c67c8477d55623cf19cb92af55db3d8
Agrim-tech7 <agrim.workplace.com@gmail.com>
2026-02-11 12:17:14 +0530

    Find greater among 2 numbers.

---
+++ greatestof2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter a=");
+    scanf("%d",&a);
+    printf("enter b=");
+    scanf("%d",&b);
+    
+    if(a>b)
+
+{
+    printf("\n %d is greater",a);
+}
+else
+{
+printf("\n %d is greater",b);
+}
+return 0;
+}
